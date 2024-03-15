308254f434542ddd8a58ed91d7a234e40a5d9efc
mikkimatt <mim23@hi.is>
2024-03-15 11:31:02 +0000

    Setja upp draft á hvernig routes væru í api.ts fyrir items og companies.

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,10 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Lagerstaða API');
+});
+
 app.use((req: Request, res: Response, next: NextFunction): void => {
   res.status(404).json({ error: "Not found" });
 });
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,58 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import {
+  listItemsInSale,
+  listItemsInCompany,
+  createItem,
+  updateItemHandler,
+  deleteItemHandler,
+} from "./items.js";
+import { 
+  listCompanyById,
+  createCompany,
+  updateCompanyById,
+  deleteCompanyById,
+} from "./companies.js";
+
 
 export const router = express.Router();
 
-router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello, world!' });
-});
+export async function index(req: Request, res: Response) {
+  return res.json([
+    {
+      href: "/company",
+      methods: ["POST"],
+    },
+    {
+      href: "/company/:id",
+      methods: ["GET", "PATCH", "DELETE"],
+    },
+    {
+      href: "/item",
+      methods: ["POST"],
+    },
+    {
+      href: "/items/sale/:saleId",
+      methods: ["GET"],
+    },
+    {
+      href: "/items/:companyId",
+      methods: ["GET", "PATCH", "DELETE"],
+    },
+  ]);
+}
+
+router.get("/", index);
+
+/* Company routes */
+
+router.get("/company/:id", listCompanyById);
+router.post("/company", createCompany);
+router.patch("/company/:id", updateCompanyById);
+router.delete("/company/:id", deleteCompanyById);
+
+/* Item routes */
+router.get("/items/sale/:saleId", listItemsInSale);
+router.get("/items/:companyId", listItemsInCompany);
+router.post("/item", createItem);
+router.patch("/item/:id", updateItemHandler);
+router.delete("/item/:id", deleteItemHandler);
